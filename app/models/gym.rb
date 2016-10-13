4b5d72cf206a31542dd8225a395d372bf2a05ce5
alexbispo <alexbispo.artes@gmail.com>
2016-10-13 08:15:27 -0300

    Formatando a exibição do horário de abertura e fechamento das academias.

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -8,4 +8,21 @@ class Gym < ApplicationRecord
   before_create do |gym|
     gym.cnpj.remove!(/\D/)
   end
+
+  def opening_hourary
+    hourary(opening_time_in_sec.to_f)
+  end
+
+  def closing_hourary
+    hourary(closing_time_in_sec.to_f)
+  end
+
+  private
+
+  def hourary(sec)
+    now = Time.current
+    mid_night = Time.new(now.year, now.month, now.day, 0, 0, 0)
+    hourary = mid_night + sec
+    hourary.to_s(:time)
+  end
 end
